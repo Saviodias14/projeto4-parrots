4d5040af6bec60ad588414b4c0218be07ec81803
Savio Dias Viana <saviodiasviana@gmail.com>
2023-01-12 11:19:08 -0300

    Jogo com a funcionalidade pronta

--- js/comando.js
+++ js/comando.js
@@ -1,5 +1,9 @@
 const gifs =['./img/bobrossparrot.gif','./img/explodyparrot.gif','./img/fiestaparrot.gif','./img/metalparrot.gif','./img/revertitparrot.gif','./img/tripletsparrot.gif','./img/unicornparrot.gif']; 
+gifs.sort(comparador);
 let numCartas;
+
+//Começo do jogo
+
 numeroDeCartas();
 function numeroDeCartas(){
     numCartas = prompt("Com quantas cartas você quer jogar?");
@@ -9,6 +13,9 @@ function numeroDeCartas(){
         numeroDeCartas();
     }
 }
+
+//Distribuição aleatória das cartas de acordo o número escolhido
+
 const containerPrincipal = document.querySelector(".container-principal");
 const cartasDistribuidas=[];
 let contador=0;
@@ -28,12 +35,67 @@ while(contador<(numCartas)){
             <img src="${gifs[gifAdicionado]}" alt="Imagem de um papagaio">
         </div>
     </div>`
-    containerPrincipal.innerHTML+=cartaAdicionadaNoDom;
     cartasDistribuidas.push(cartaAdicionadaNoDom);
     contador++;
-    console.log(cartaAdicionadaNoDom);
+}
+cartasDistribuidas.sort(comparador);
+contador=0;
+while(contador<cartasDistribuidas.length){
+    containerPrincipal.innerHTML+=cartasDistribuidas[contador];
+    contador++;
+}
+
+//Virando as cartas
+
+contador=0;
+let espera = 0;
+function primeiraCarta(carta){
+    carta.classList.add("vira");
+    contador++;
+}
+function segundaCarta(carta){
+    espera++;
+    carta.classList.add("vira");
+    contador = 0;
+    const cartasViradas = document.querySelectorAll('.vira');
+    console.log(cartasViradas);
+    if(cartasViradas[0].innerHTML===cartasViradas[1].innerHTML){
+        console.log('São Iguais');
+        cartasViradas[0].classList.add('cartasIguais');
+        cartasViradas[1].classList.add('cartasIguais');
+        cartasViradas[0].classList.remove('vira');
+        cartasViradas[1].classList.remove('vira');
+        espera=0;
+    }
+    else{
+        setTimeout(function(){cartasViradas[0].classList.remove("vira");}, 1000);
+        setTimeout(function(){cartasViradas[1].classList.remove("vira");}, 1000);
+        setTimeout(function(){espera=0;}, 1000);
+    }
 }
 function viraCarta(carta){
-    carta.classList.add("vira");
-    setTimeout(function(){carta.classList.remove("vira");}, 2000);
+    if(espera!==0){
+        return;
+    }
+    if(contador===0){
+        primeiraCarta(carta);
+    }
+    else{
+        segundaCarta(carta);
+    }
+}
+//function viraCarta(carta){
+  //  carta.classList.add("vira");
+   // setTimeout(function(){carta.classList.remove("vira");}, 2000);
+//}
+
+
+
+
+
+
+
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
