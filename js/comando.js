662c1faa00da71337d6721b514a96f5fdddd370f
Savio Dias Viana <saviodiasviana@gmail.com>
2023-01-13 12:02:24 -0300

    Adição do avaliador HUB

--- js/comando.js
+++ js/comando.js
@@ -2,9 +2,7 @@ const gifs =['./img/bobrossparrot.gif','./img/explodyparrot.gif','./img/fiestapa
 gifs.sort(comparador);
 let numCartas;
 const segundo = document.querySelector('.segundo');
-const minuto = document.querySelector('.minuto');
 let segundoTempo = 0;
-let minutoTempo = 0;
 let cron;
 //Começo do jogo
 
@@ -13,7 +11,6 @@ function numeroDeCartas(){
     numCartas = prompt("Com quantas cartas você quer jogar?");
     Number(numCartas);
     if(numCartas<4||(numCartas%2)!==0||numCartas>14){
-        alert("Você deve escolher um número par entre 4 e 14!");
         numeroDeCartas();
     }
 }
@@ -33,12 +30,12 @@ function distribuiCartasAleatorias(){
             gifAdicionado=(contador-1)/2;
         }
     
-         const cartaAdicionadaNoDom = `<div onclick="viraCarta(this)" class="card">
+         const cartaAdicionadaNoDom = `<div onclick="viraCarta(this)" data-test="card" class="card">
              <div class="front-face face">
-                 <img src="./img/back.png" alt="Imagem de um papagaio">
+                 <img data-test="face-down-image" src="./img/back.png" alt="Imagem de um papagaio">
              </div>
              <div class="back-face face">
-                 <img src="${gifs[gifAdicionado]}" alt="Imagem de um papagaio">
+                 <img data-test="face-up-image" src="${gifs[gifAdicionado]}" alt="Imagem de um papagaio">
              </div>
          </div>`
          cartasDistribuidas.push(cartaAdicionadaNoDom);
@@ -98,7 +95,7 @@ function viraCarta(carta){
 
 function fimDoJogo(){
     clearInterval(cron);
-    alert(`Você ganhou em ${numeroDeJogadas} jogadas! A duração do jogo foi de ${contagem(Number(minutoTempo))}:${contagem(Number(segundoTempo))} segundos!`);
+    alert(`Você ganhou em ${numeroDeJogadas} jogadas! A duração do jogo foi de ${Number(segundoTempo)} segundos!`);
     let reiniciarJogo = prompt('Quer jogar novamente? sim ou não?');
     while((reiniciarJogo!=='sim')&&(reiniciarJogo!=='não')){
         reiniciarJogo = prompt('Quer jogar novamente? sim ou não?');
@@ -109,9 +106,7 @@ function fimDoJogo(){
     //Reinicia o jogo
     if(reiniciarJogo==='sim'){
         segundoTempo = 0;
-        minutoTempo = 0;
         segundo.innerHTML = '00';
-        minuto.innerHTML = '00';
         const conteudoDoDom = document.querySelector('.container-principal').querySelectorAll('div');
         for(let indice=0;indice<conteudoDoDom.length;indice++){
             conteudoDoDom[indice].remove();
@@ -133,17 +128,8 @@ function start() {
   }
 function iniciaTimer(){
     segundoTempo++;
-    if(Number(segundoTempo)===60){
-        segundoTempo = 0;
-        minutoTempo++;
-    }
-    segundoTempo = contagem(Number(segundoTempo));
-    minutoTempo = contagem(Number(minutoTempo));
-    segundo.innerHTML = segundoTempo;
-    minuto.innerHTML = minutoTempo;
-}
-function contagem(valor){
-    return valor >= 10 ? valor : `0${valor}`;
+    segundoTempo = Number(segundoTempo);
+    segundo.innerHTML = `${segundoTempo}`;
 }
 
 function comparador() { 
